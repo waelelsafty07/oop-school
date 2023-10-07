a41a98fcd2ea4f8aacb27dbba632132d336b58bc
waelelsafty07 <waelelsafty07@gmail.com>
2023-10-07 19:40:51 +0300

    Fix associations

--- person.rb
+++ person.rb
@@ -1,34 +1,38 @@
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :age, :name, :rental
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..188)
-    @name = name
     @age = age
+    @name = name
+    @id = Random.rand(1...1000)
     @parent_permission = parent_permission
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals << rental
-    rental.person = self
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
+  # getters for id
+  def gets_id
+    @id
   end
 
   def correct_name
     @name
   end
 
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
-    @age >= 18
+    age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 end
