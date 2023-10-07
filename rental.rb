e59502d009290166b4af411de18147664e4f0acc
waelelsafty07 <waelelsafty07@gmail.com>
2023-10-08 00:20:21 +0300

    fix rental

--- rental.rb
+++ rental.rb
@@ -1,20 +1,13 @@
+require_relative 'book'
+
 class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
-    @book = book
     @person = person
-
-  end
-
-  def add_book(book)
     @book = book
-    book.rentals << self unless book.rentals.include?(self)
-  end
-
-  def add_person(person)
-    @person = person
-    person.rentals << self unless person.rentals.include?(self)
+    person&.add_rental(self)
+    book&.add_rental(self)
   end
 end
